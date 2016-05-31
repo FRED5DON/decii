587f402bd17dd0f3de4a200511e1a1d710ed400c
freddon <gsiner@live.com>
2016-05-31 19:28:32 +0800

    add demo of md;

--- public/templates/publish.php
+++ public/templates/publish.php
@@ -26,36 +26,51 @@ $map = include_once 'plates/lang.php';
         <div class="col-sm-8 col-md-6 center-block panel-sign">
             <form class="form-horizontal">
                 <input name="title" type="text" placeholder="Title?" />
-                <textarea id="content" name="content" data-provide="markdown" rows="10"></textarea>
+                <textarea id="content" name="content" data-provide="markdown" rows="10">
+                    ###Head
+                </textarea>
                 <label class="checkbox">
                     <input name="publish" type="checkbox"> Publish
                 </label>
                 <hr/>
-                <button type="submit" class="btn">Submit</button>
+                <button type="submit" class="btn" >Submit</button>
             </form>
 
 
-            <div data-provide="markdown-editable">
-                <h3>This is some editable heading</h3>
-                <p>Well, actually all contents within this "markdown-editable" context is really editable. Just click anywhere!</p>
-            </div>
-            </div>
+            <div data-provide="markdown-editable" id="content-md">
 
+            </div>
+        </div>
     </div><!-- /.row -->
+    <button id="fore-see">预览</button>
 
 </div><!-- /.container -->
 
 <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
 <script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
-<?php include_once 'plates/footer.php'?>
 
+<?php include_once 'plates/footer.php'?>
+<script src="<?=$path_dir_templates?>scripts/markdown.js"></script>
+<script src="<?=$path_dir_templates?>scripts/to-markdown.js"></script>
+<script src="<?=$path_dir_templates?>scripts/bootstrap-markdown.js"></script>
 <script>
 
-    $(document).ready(function(){
+//    $(document).ready(function(){
+//
+////        $('#fore-see').click(function(){
+////
+////            $('#content-md').html($("#content").val());
+////            $('#content-md').markdown();
+////            $("#content").attr('data-provide','markdown-editable');
+////
+////        });
+//
+//    });
 
-        $("#content").markdown({autofocus:false,savable:false})
-    });
+    ;(function($){
+        $("#content").markdown({autofocus:false,savable:true});
+    }(jQuery))
 
 </script>
 
